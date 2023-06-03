6d38c77932567bb3e9282e5904a6b6c1e7ced068
poochingy <jliu6162@gmail.com>
2023-06-03 12:50:07 -0400

    state vars work now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import "./login.css"
+import fetchUserSignup from "./userfetches";
+
 function LoginUI() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
     const changeUsername = (event) => {
@@ -15,6 +18,11 @@ function LoginUI() {
         setPassword(event.target.value);
 
     }
+
+    const changeRole = (event) => {
+        setRole(event.target.value);
+    }
+
 
     let registerMode = false;
     const switchMode = (event) => {
@@ -47,14 +55,6 @@ function LoginUI() {
 
         event.preventDefault()
 
-        //check if email is email format (regex)
-        let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(registerMode && !email.value.match(mailFormat)){
-            setErrorMessage("Invalid e-mail format");
-
-            return;
-        }
-
         if(registerMode && password.value.length < 8 && registerMode){
             setErrorMessage("Password must be at least 8 characters");
 
@@ -68,30 +68,37 @@ function LoginUI() {
         }
         if(registerMode) {
             setErrorMessage('');
-            fetchUserSignup(username, password);
+
+            fetchUserSignup(username, password, role);
         } else {
             setErrorMessage('');
 
-            fetchUserLogin(username, password)
+            // fetchUserLogin(username, password)
         }
     }
 
-    function fetchUserSignup(email, password) {
-
+    function logvalues(){
+        console.log(username);
+        console.log(password);
+        console.log(role);
     }
 
-    function fetchUserLogin(email, password) {
-
-    }
 
     return (
         <div id="container">
             <div id="logo">
                 <h1 style={{fontFamily: "Qanelas Soft SemiBold", fontSize: "400%"}}>jun 3</h1>
             </div>
-            <input id="email" className="input" type="text" name="email" placeholder="E-mail" value={username} onChange={changeUsername}></input>
-            <input id="password" className="input" type="password" name="password" placeholder="Password" value={password} onChange={changePassword}></input>
-            <p id="errorOutput"></p>
+            <input id={"username"} className="input" type="text" name="username" placeholder="Username" value={username} onChange={changeUsername}></input>
+            <input id={"password"} className="input" type="password" name="password" placeholder="Password" value={password} onChange={changePassword}></input>
+            <input id={"role"} className={"input"} list={"roles"} name="role" placeholder="Role" value={role} onChange={changeRole}/>
+            <dataList id={"roles"}>
+                <option value={"Teacher"}></option>
+                <option value={"Student"}></option>
+            </dataList>
+            {/*check if is error message*/}
+            {errorMessage && <p>{errorMessage}</p>}
+            <button className={"button"} onClick={logvalues}>testing</button>
             <button id="login" className="button" onClick={validateLoginForm}>Login</button>
             <button id="register" className="button" onClick={switchMode}>Register</button>
         </div>
--- src/userfetches.js
+++ src/userfetches.js
@@ -1,4 +1,4 @@
-function fetchUserSignup(email, password){
+function fetchUserSignup(email, password, role){
     fetch("http://10.0.0.74:8080/user/signup", {
         method: "POST",
         headers: {
@@ -6,7 +6,8 @@ function fetchUserSignup(email, password){
         },
         body: JSON.stringify({
             "email": String(email), //receive from signup page
-            "password": String(password) //receive from signup page
+            "password": String(password), //receive from signup page
+            "role": String(role) // receive from signup page
         })
     }).then(response => {
         return response.json()
@@ -16,41 +17,42 @@ function fetchUserSignup(email, password){
     console.log(JSON.stringify({
         email: String(email), //receive from signup page
         password: String(password), //receive from signup page
+        role: String(role)
     }))
 }
 
 export default fetchUserSignup;
 //
-// let loginReady = false;
-// function fetchUserLogin(email, password) {
-//     return new Promise((resolve, reject) => {
-//         let loginReady = false;
-//         fetch("http://10.0.0.74:8080/user/login", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 "email": String(email),
-//                 "password": String(password),
-//             })
-//         })
-//             .then(response => response.json())
-//             .then(data => {
-//                 const token = data.jwtToken;
-//                 document.cookie = `token=${token}; path=/`;
-//                 loginReady = true;
-//                 let currentUser = new User(String(email), String(password), token)
-//                 localStorage.setItem("user", JSON.stringify(currentUser));
-//                 resolve(loginReady);
-//             })
-//             .catch(error => {
-//                 loginReady = false;
-//                 console.log(error)
-//                 reject(error);
-//             });
-//     });
-// }
+let loginReady = false;
+function fetchUserLogin(email, password) {
+    return new Promise((resolve, reject) => {
+        let loginReady = false;
+        fetch("http://10.0.0.74:8080/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "email": String(email),
+                "password": String(password),
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                const token = data.jwtToken;
+                document.cookie = `token=${token}; path=/`;
+                loginReady = true;
+                let currentUser = new User(String(email), String(password), token)
+                localStorage.setItem("user", JSON.stringify(currentUser));
+                resolve(loginReady);
+            })
+            .catch(error => {
+                loginReady = false;
+                console.log(error)
+                reject(error);
+            });
+    });
+}
 //
 // function fetchUserLogout(){
 //     fetch("http://10.0.0.74:8080/user/logout", {
