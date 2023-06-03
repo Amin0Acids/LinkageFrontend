8113150ea7c2086094d00323455df7b6f005ef61
poochingy <jliu6162@gmail.com>
2023-06-03 10:37:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-function LoadUI() {
+function loginUI() {
     return (
         <body>
-        <div id="container"> <!-- This is the container for the logo and the form, and centers the elements -->
+        <div id="container">
             <div id="logo">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                    <img src='' alt="no picture"
-                         style="width: 125px; height: auto; margin-right: 20px;"></img>
-                </a>
-
-                <h1 style="font-family: 'Qanelas Soft SemiBold'; font-size:400%">jun 3</h1>
+                <h1 style={{fontFamily: "Qanelas Soft SemiBold", fontSize: 400%}}>jun 3</h1>
             </div>
-
-            <!-- login form -->
-
-
             <input id="email" className="input" type="text" name="email" placeholder="E-mail">
                 <input id="password" className="input" type="password" name="password" placeholder="Password">
                     <p id="errorOutput"></p>
