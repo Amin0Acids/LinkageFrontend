b2d7b85d40cddcf5add8dba6799f729c797c1fad
poochingy <jliu6162@gmail.com>
2023-06-03 10:31:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import logo from "./logo.svg";
-function App() {
+function LoadUI() {
     return (
-        <div className="App">
-            Hello
+        <body>
+        <div id="container"> <!-- This is the container for the logo and the form, and centers the elements -->
+            <div id="logo">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    <img src='' alt="no picture"
+                         style="width: 125px; height: auto; margin-right: 20px;"></img>
+                </a>
+
+                <h1 style="font-family: 'Qanelas Soft SemiBold'; font-size:400%">jun 3</h1>
+            </div>
+
+            <!-- login form -->
+
+
+            <input id="email" className="input" type="text" name="email" placeholder="E-mail">
+                <input id="password" className="input" type="password" name="password" placeholder="Password">
+                    <p id="errorOutput"></p>
+                    <button id="login" className="button" onClick="validateLoginForm()">Login</button>
+                    <button id="register" className="button" onClick="switchMode()">Register</button>
+                </input>
+            </input>
         </div>
+        </body>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<LoadUI />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
